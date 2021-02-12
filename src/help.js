a842330dad435e93e2cbf31fc5b8eab86144fd8b
jaoflex <joaoetobi153@gmail.com>
2021-02-12 11:58:30 -0300

    Frist commit

---
+++ src/help.js
@@ -0,0 +1,60 @@
+╔══✪〘 INFO 〙✪══
+║
+╠➥ 🖤🥀𝐁𝚯𝐓 𝐉𝐀̃𝐎 𝐅𝐋𝐄𝐗🖤🥀
+╠➥ 1.0
+╠➥ 𝐃𝐎𝐍𝐎: ⁰̷¹̷⁰̷¹̷𝖏̷𝖆̷𝖔̷ ̷𝖋̷𝖑̷𝖊̷𝖝̷
+╠➥ wa.me/+557791235791
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+╠══✪〘 MENU 〙✪══
+║
+║1 ${prefix}sticker ou ${prefix}stiker
+║função:transorma imagem ou gif em figurinha
+║2 ${prefix}toimg
+║função:transforma figurinha em imagem
+║3 ${prefix}sticker nobg ou ${prefix}stiker nobg
+║função:transforma imagem em figurinha removendo o fundo
+║            𝖒𝖊𝖒𝖊𝖘 𝖈𝖔𝖒𝖆𝖓𝖉𝖔𝖘
+║4 ${prefix}memeindo
+║função:imagens aleatórias de meme [indo]
+║5$ {prefix} meme
+║função:imagens aleatórias de meme [inglês]
+║             𝖔𝖚𝖙𝖗𝖔𝖘 𝖈𝖔𝖒𝖆𝖓𝖉𝖔𝖘
+║6 ${prefix}gtts
+║função:converte texto em audio
+║7 ${prefix}loli [on]
+║função: fotos aleatorias de lolis
+║8 ${prefix}nsfwloli [off]
+║funçoes: hentaiKKKKKK
+║9 ${prefix}url2img
+║função:tirar screenshots da web
+║10 ${prefix}leens [na legenda]
+║11 ${prefix}wait [na legenda]
+║função:pesquisar anime com imagem [What Anime Is This / That]
+║12 ${prefix}setprefix
+║função:substituir prefixo
+║            𝖈𝖔𝖒𝖆𝖓𝖉𝖔𝖘 𝖉𝖔 𝖌𝖗𝖚𝖕𝖔
+║13 ${prefix}add
+║função:adicionar membro ao grupo
+║14  ${prefix}kick
+║função:tirar membros do grupo
+║15 ${prefix}promote
+║função:tornar o membro do grupo como administrador do grupo
+║16 ${prefix}demote
+║função:tornar o administrador do grupo como membro do grupo
+║17 ${prefix}linkgroup
+║função:pegue o link do grupo
+║18  ${prefix}leave
+║descrição:Faça o bot sair do grupo obs: função so para adms
+║19  ${prefix}tagall
+║função:marca todos os membros do grupo, incluindo administradores também
+║20  ${prefix}simih
+║função:ative o modo simi no grupo
+║21 ${prefix}simih 0
+║função:desativar simi simi
+╠══✪〘 𝗗𝗢𝗡𝗢 〙✪══
+║
+║ NOME: 🖤🥀𝐁𝚯𝐓 𝐉𝐀̃𝐎 𝐅𝐋𝐄𝐗🖤🥀
+║ INSTA: @jaorodrigues._
+║ WPP: wa.me/+557791235791
+║ 
+╚═〘 🖤🥀𝐁𝚯𝐓 𝐉𝐀̃𝐎 𝐅𝐋𝐄𝐗🖤🥀 〙`
